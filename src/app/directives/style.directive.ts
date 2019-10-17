56f3412110614c4267ed290460990ed6d08fe903
hmelii <anufry@inbox.ru>
2019-10-17 09:53:51 +0300

    angular 5

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,11 +1,35 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, Input } from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
 })
 export class StyleDirective {
+
+    @Input('appStyle') color: string = 'blue'
+    @Input('fontWeight') fontWeight: string = 'normal'
+
     constructor(private el: ElementRef, private renderer: Renderer2) {
-        this.renderer.setStyle(el.nativeElement, 'color',  'blue') // способ правильный
+        //this.renderer.setStyle(el.nativeElement, 'color',  'blue') // способ правильный
         //el.nativeElement.style.color = 'red' // способ некорректный назначения стиля
+    }
+
+    @HostListener('click', ['$event'])
+    onClick(event: Event) { // событие клика 
+        console.log(event)
+    }
+
+    @HostListener('mouseover', ['$event'])
+    onMouseOver(event: Event) { 
+        console.log(event)
+        //this.renderer.setStyle(this.el.nativeElement, 'color',  'blue')
+        this.renderer.setStyle(this.el.nativeElement, 'color',  this.color) // берем значение из передданого параметра
+        this.renderer.setStyle(this.el.nativeElement, 'fontWeight',  this.fontWeight)
+    }
+
+    @HostListener('mouseout', ['$event'])
+    onMouseOut(event: Event) { 
+        console.log(event)
+        this.renderer.setStyle(this.el.nativeElement, 'color',  '')
+        this.renderer.setStyle(this.el.nativeElement, 'fontWeight',  '')
     }
 }
